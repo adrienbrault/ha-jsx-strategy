596250f7f53332c8c06b31df45aa18cccde09ad2
Adrien Brault <adrien.brault@gmail.com>
2023-12-15 02:57:27 +0100

    feat: example and instructions

--- README.md
+++ README.md
@@ -4,12 +4,24 @@ Goal: Use https://developers.home-assistant.io/docs/frontend/custom-ui/custom-st
 
 In my case, I want to do something like https://github.com/AalianKhan/mushroom-strategy, but with React and my own config/cards.
 
-TODO:
-- HA Strategy part
-- IntrinsicElements to get props/etc autocompletion
-    - for built in cards
-    - for HACS cards
-- dynamic config based on entities from areas
+## Installation
+
+[First, install bun](https://bun.sh).
+
+Then:
+```
+git clone https://github.com/adrienbrault/jsx-strategy.git www/jsx-strategy
+```
+
+Customize `www/jsx-strategy/src/index.tsx` to your needs.
+Use VSCode for autocompletion and type checking!
+
+Every time you edit this file, make sure to run:
+```
+bun build www/jsx-strategy/src/index.tsx > www/jsx-strategy/index.js
+```
+
+[Add a dashboard resource](https://my.home-assistant.io/redirect/lovelace_resources/) with url `/local/jsx-strategy/index.js` as a `JavaScript Module`.
 
 ## Development
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
 import { Area, Device, Entity } from "./config-types";
 import { elementToConfig } from "./jsx";
 
+// Create your own components
+const AreaWithLights = ({
+  area,
+  entities,
+}: {
+  area: Area;
+  entities: Array<Entity>;
+}) => {
+  const lightEntities = entities.filter((entity) =>
+    entity.entity_id.startsWith("light.")
+  );
+
+  return (
+    <vertical-stack>
+      <custom-mushroom-title
+        title={`${area.name} - ${area.area_id}`}
+      ></custom-mushroom-title>
+      {...lightEntities.map((entity) => {
+        return <custom-mushroom-light entity={entity.entity_id} />;
+      })}
+    </vertical-stack>
+  );
+};
+
 class JsxStrategy {
   static async generateDashboard(info) {
     const [areas, devices, entities]: [
@@ -13,24 +37,24 @@ class JsxStrategy {
       info.hass.callWS({ type: "config/entity_registry/list" }),
     ]);
 
-    console.log({ areas, devices, entities });
+    console.log("JSX Strategy state: ", { areas, devices, entities });
     const config = {
       title: "Generated Dashboard",
       views: [
         {
-          cards: areas.map((area) => {
-            return (
-              <markdown
-                content={`${area.name} - ${
-                  area.area_id
-                }\nGenerated at ${new Date().toLocaleString()}`}
-              ></markdown>
-            );
-          }),
+          cards: areas.map((area) => (
+            // Use your component
+            <AreaWithLights
+              area={area}
+              entities={entities.filter(
+                (entity) => entity.area_id === area.area_id
+              )}
+            />
+          )),
         },
       ],
     };
-    console.log({ config });
+    console.log("JSX Strategy generated config: ", { config });
     return config;
   }
 }
